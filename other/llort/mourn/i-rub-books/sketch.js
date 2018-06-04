f8d1cb362c7439450bc08d47ef1d79dab1a1ef72
Vince14Genius <vince14genius@icloud.com>
2018-06-04 17:43:03 +0800

    updated mourning simulator

--- other/llort/mourn/i-rub-books/sketch.js
+++ other/llort/mourn/i-rub-books/sketch.js
@@ -1,13 +1,11 @@
-let frameTime = 0 // Time at which the current frame is updated
-let oldTime = 0 // Time at which the previous frame is updated
-let fpsCooldown = 0 // When fpsCooldown <= 0, set displayFPS = fps
-let fps = 1 // Ratio of (1000ms) to (difference between frameTime and oldTime), MUST START WITH 1
-let displayFPS = 0 // fps displayed on screen, updated every second
+var frameTimes = []
+var oldTime = 0
+var fpsCooldown = 0 // When fpsCooldown <= 0, set displayFPS = fps
+var displayFPS = 0 // fps displayed on screen, updated every second
 
 var frameWidth = 400
 var frameHeight = 400
 
-var paused = true
 var muted = false
 
 var amplitude
@@ -23,6 +21,8 @@ var buttonKaraoke
 var imageDian
 var imagesSB = []
 var sbScale = 0
+
+var pauseDrawn = false // this variable is used to reduce draw calls during pause times
 
 class GifContainer {
     constructor(scale, updateFunc) {
@@ -50,26 +50,37 @@ class GifContainer {
             } else {
                 this.y = this.destinationY
             }
-        } else if (paused) {
+        } else if (!currentSong.isPlaying()) {
             this.image.hide()
             image(this.static, this.x, this.y, this.static.width * this.scale, this.static.height * this.scale)
         } else {
             this.image.show()
             this.image.size(this.static.width * this.scale, this.static.height * this.scale)
-
-            if (this.destinationX !== null) {
-                this.x += (this.destinationX - this.x) * 0.2
-                if (this.x > this.destinationX - 5 && this.x < this.destinationX + 5) {
-                    this.x = this.destinationX
-                }
-            }
-            if (this.destinationY !== null) {
-                this.y += (this.destinationY - this.y) * 0.2
-                if (this.y > this.destinationY - 5 && this.y < this.destinationY + 5) {
-                    this.y = this.destinationY
-                }
-            }
+            this.moveTowardsXDestination()
+            this.moveTowardsYDestination()
             this.image.position(this.x, this.y)
+        }
+    }
+
+    groundY() {
+        this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    }
+
+    moveTowardsXDestination() {
+        if (this.destinationX !== null) {
+            this.x += (this.destinationX - this.x) * 0.2
+            if (this.x > this.destinationX - 5 && this.x < this.destinationX + 5) {
+                this.x = this.destinationX
+            }
+        }
+    }
+
+    moveTowardsYDestination() {
+        if (this.destinationY !== null) {
+            this.y += (this.destinationY - this.y) * 0.2
+            if (this.y > this.destinationY - 5 && this.y < this.destinationY + 5) {
+                this.y = this.destinationY
+            }
         }
     }
 
@@ -87,28 +98,40 @@ class GifContainer {
 }
 
 var gifB = new GifContainer(1, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.checkSetDestinationXBetweenMargins(-40, -40)
     this.standardUpdate()
 })
 
 var gifC = new GifContainer(1, function () {
     this.checkSetDestinationXBetweenMargins(30, 30)
-    this.checkSetDestinationYBetweenMargins(10, 60)
+    if (currentSong.isPlaying()) {
+        this.checkSetDestinationYBetweenMargins(10, 60)
+    } else {
+        this.groundY()
+        this.moveTowardsYDestination()
+        this.image.position(this.x, this.y)
+    }
     this.standardUpdate()
 })
 
 var gifD = new GifContainer(1, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.destinationX = windowWidth / 2 - this.static.width * this.scale / 2
     this.standardUpdate()
 })
 
 var gifE = new GifContainer(0.4, function () {
     this.checkSetDestinationXBetweenMargins(10, 10)
-    this.checkSetDestinationYBetweenMargins(10, 30)
+    if (currentSong.isPlaying()) {
+        this.checkSetDestinationYBetweenMargins(10, 30)
+    } else {
+        this.groundY()
+        this.moveTowardsYDestination()
+        this.image.position(this.x, this.y)
+    }
     this.standardUpdate()
-    if (!paused) {
+    if (currentSong.isPlaying()) {
         this.animationPhase += 23
         if (this.animationPhase > 360) {
             this.animationPhase %= 360
@@ -118,25 +141,25 @@ var gifE = new GifContainer(0.4, function () {
 })
 
 var gifF = new GifContainer(0.5, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.checkSetDestinationXBetweenMargins(240, -30)
     this.standardUpdate()
 })
 
 var gifG = new GifContainer(0.5, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.checkSetDestinationXBetweenMargins(-60, 60)
     this.standardUpdate()
 })
 
 var gifH = new GifContainer(1, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.destinationX = windowWidth / 2 - frameWidth + this.static.width * this.scale / 2 + 160
     this.standardUpdate()
 })
 
 var gifI = new GifContainer(0.75, function () {
-    this.destinationY = 160 + frameHeight - this.static.height * this.scale
+    this.groundY()
     this.checkSetDestinationXBetweenMargins(240, -30)
     this.standardUpdate()
 })
@@ -155,7 +178,8 @@ function preload() {
     gifH.static = loadImage('images/h.gif')
     gifI.static = loadImage('images/i.gif')
 
-    for (var i = 1; i <= 2; i++) {
+    // Images for sb.
+    for (var i = 1; i <= 3; i++) {
         imagesSB.push(loadImage(`images/sb-${i}.png`))
     }
 
@@ -164,6 +188,7 @@ function preload() {
 
 function setup() {
     // setup() runs once. Put your setup code here.
+
     createCanvas(windowWidth, windowHeight)
 
     currentSong = defaultSong
@@ -183,23 +208,14 @@ function setup() {
     gifH.image = createImg('images/h.gif')
     gifI.image = createImg('images/i.gif')
     gifD.image = createImg('images/d.gif')
-
 }
 
 function draw() {
     // draw() runs every time before a new frame is rendered. 
 
-    // Time updates
-
-    if (!paused && Math.random() < 0.01) {
+    if (currentSong.isPlaying() && Math.random() < 0.01) {
         nextPhoto()
     }
-
-    frameTime = millis()
-    const timeDiff = frameTime - oldTime
-    fps = (1000 / (timeDiff) + fps * (fps - 1)) / fps
-    textSize(32)
-    fpsCooldown -= timeDiff
 
     // Update
 
@@ -208,7 +224,7 @@ function draw() {
     noStroke()
 
     var volume = 0
-    if (!paused) {
+    if (currentSong.isPlaying()) {
         volume = amplitude.getLevel()
     }
 
@@ -221,82 +237,105 @@ function draw() {
         sbScale = visualVolume
     }
 
-    image(imagesSB[0], windowWidth / 2 - frameWidth / 2 - frameWidth / 4 * sbScale, 160 - frameHeight / 4 * sbScale, frameWidth + frameWidth / 2 * sbScale, frameHeight + frameHeight / 2 * sbScale)
-    filter(GRAY)
-    tint(255, sbScale * 255)
-    image(imagesSB[0], windowWidth / 2 - frameWidth / 2 - frameWidth / 4 * sbScale, 160 - frameHeight / 4 * sbScale, frameWidth + frameWidth / 2 * sbScale, frameHeight + frameHeight / 2 * sbScale)
-    noTint()
+    var willDraw = true
 
-    // Overlay
-
-    fill(20)
-    rect(0, 0, windowWidth / 2 - frameWidth / 2, windowHeight)
-    rect(windowWidth / 2 + frameWidth / 2, 0, windowWidth / 2 - frameWidth / 2, windowHeight)
-    rect(0, 0, windowWidth, 160)
-    rect(0, 160 + frameHeight, windowWidth, windowHeight - (160 + frameHeight))
-
-    // Objects
-
-    fill(40)
-    rect(windowWidth / 2 - frameWidth / 2 - 10, 200, -160, frameHeight - 20)
-    rect(windowWidth / 2 + frameWidth / 2 + 10, 200, 160, frameHeight - 20)
-
-    fill(20)
-    stroke(150)
-    strokeWeight(16)
-    ellipse(windowWidth / 2 - frameWidth / 2 - 90, 300, 100 + 40 * sbScale, 100 + 40 * sbScale)
-    ellipse(windowWidth / 2 - frameWidth / 2 - 90, 480, 80 + 32 * sbScale, 75 + 32 * sbScale)
-    ellipse(windowWidth / 2 + frameWidth / 2 + 90, 300, 100 + 40 * sbScale, 100 + 40 * sbScale)
-    ellipse(windowWidth / 2 + frameWidth / 2 + 90, 480, 80 + 32 * sbScale, 75 + 32 * sbScale)
-    noStroke()
-
-    fill(64)
-    textSize(48)
-    textAlign(CENTER, TOP)
-    text("沉 痛", windowWidth / 2 - frameWidth / 2 - 90, 140)
-    text("悼 念", windowWidth / 2 + frameWidth / 2 + 90, 140)
-
-    for (gifContainer of allGifs) {
-        gifContainer.update()
+    if (currentSong.isPlaying || sbScale !== 0) {
+        pauseDrawn = false
+    } else if (pauseDrawn) {
+        willDraw = false
     }
 
-    if (paused) {
+    if (willDraw) {
+        image(imagesSB[0], windowWidth / 2 - frameWidth / 2 - frameWidth / 4 * sbScale, 160 - frameHeight / 4 * sbScale, frameWidth + frameWidth / 2 * sbScale, frameHeight + frameHeight / 2 * sbScale)
         filter(GRAY)
-        image(imageDian, windowWidth / 2 - 80, 160 + frameHeight / 2, 160, 160)
+        tint(255, sbScale * 255)
+        image(imagesSB[0], windowWidth / 2 - frameWidth / 2 - frameWidth / 4 * sbScale, 160 - frameHeight / 4 * sbScale, frameWidth + frameWidth / 2 * sbScale, frameHeight + frameHeight / 2 * sbScale)
+        noTint()
+    
+        // Overlay
+    
+        fill(20)
+        rect(0, 0, windowWidth / 2 - frameWidth / 2, windowHeight)
+        rect(windowWidth / 2 + frameWidth / 2, 0, windowWidth / 2 - frameWidth / 2, windowHeight)
+        rect(0, 0, windowWidth, 160)
+        rect(0, 160 + frameHeight, windowWidth, windowHeight - (160 + frameHeight))
+    
+        // Objects
+    
+        fill(40)
+        rect(windowWidth / 2 - frameWidth / 2 - 10, 200, -160, frameHeight - 20)
+        rect(windowWidth / 2 + frameWidth / 2 + 10, 200, 160, frameHeight - 20)
+    
+        fill(20)
+        stroke(150)
+        strokeWeight(16)
+        ellipse(windowWidth / 2 - frameWidth / 2 - 90, 300, 100 + 40 * sbScale, 100 + 40 * sbScale)
+        ellipse(windowWidth / 2 - frameWidth / 2 - 90, 480, 80 + 32 * sbScale, 75 + 32 * sbScale)
+        ellipse(windowWidth / 2 + frameWidth / 2 + 90, 300, 100 + 40 * sbScale, 100 + 40 * sbScale)
+        ellipse(windowWidth / 2 + frameWidth / 2 + 90, 480, 80 + 32 * sbScale, 75 + 32 * sbScale)
+        noStroke()
+    
+        fill(64)
+        textSize(48)
+        textAlign(CENTER, TOP)
+        text("沉 痛", windowWidth / 2 - frameWidth / 2 - 90, 140)
+        text("悼 念", windowWidth / 2 + frameWidth / 2 + 90, 140)
+    
+        for (gifContainer of allGifs) {
+            gifContainer.update()
+        }
+    
+        if (!currentSong.isPlaying()) {
+            filter(GRAY)
+            image(imageDian, windowWidth / 2 - 80, 160 + frameHeight / 2, 160, 160)
+        }
+    
+        // Text
+    
+        fill(0)
+        rect(windowWidth / 2 - 400, 160, -112, frameHeight)
+        rect(windowWidth / 2 + 400, 160, 112, frameHeight)
+    
+        fill(255, 0, 0)
+        textSize(64)
+    
+        stroke(255, 255, 255, 30)
+        textAlign(CENTER, TOP)
+        text("崔璁远的奇妙灵堂", windowWidth / 2, 40)
+    
+        noStroke()
+        textSize(72)
+    
+        textAlign(RIGHT, TOP)
+        text("香\n消\n玉\n殒", windowWidth / 2 - 420, 180)
+    
+        textAlign(LEFT, TOP)
+        text("梦\n断\n北\n堂", windowWidth / 2 + 420, 180)
+    
+        textSize(32)
+        textAlign(CENTER, TOP)
+        fill(100, 0, 0)
+        text("灵堂模拟器 p5.js版 v0.1 beta, powered by Wādogēmu! Media Player", windowWidth / 2, 160 + frameHeight + 100)    
     }
 
-    // Text
-
-    fill(0)
-    rect(windowWidth / 2 - 400, 160, -112, frameHeight)
-    rect(windowWidth / 2 + 400, 160, 112, frameHeight)
-
-    fill(255, 0, 0)
-    textSize(64)
-
-    stroke(255, 255, 255, 30)
-    textAlign(CENTER, TOP)
-    text("[SB_NAME]的奇妙灵堂", windowWidth / 2, 40)
-
-    noStroke()
-    textSize(72)
-
-    textAlign(RIGHT, TOP)
-    text("香\n消\n玉\n殒", windowWidth / 2 - 420, 180)
-
-    textAlign(LEFT, TOP)
-    text("梦\n断\n北\n堂", windowWidth / 2 + 420, 180)
-
-    textSize(32)
-    textAlign(CENTER, TOP)
-    fill(100, 0, 0)
-    text("灵堂模拟器 p5.js版 v0.1 beta, powered by Wādogēmu! Media Player", windowWidth / 2, 160 + frameHeight + 100)
-
+    if (currentSong.isPlaying()) {
+        buttonPlayPause.style.background = "url('images/button-pause.svg')"
+        buttonLoop.style.opacity = 0.8
+    } else {
+        buttonPlayPause.style.background = "url('images/button-play.svg')"
+        buttonLoop.style.opacity = 0.3
+    }
 
     // Display FPS
-
+    var currentTime = millis()
+    frameTimes.push(currentTime)
+    while (frameTimes.length > 0 && frameTimes[0] < currentTime - 1000) {
+        frameTimes.splice(0, 1)
+    }
+    
+    fpsCooldown -= currentTime - oldTime
     if (fpsCooldown <= 0) {
-        displayFPS = fps
+        displayFPS = frameTimes.length
         fpsCooldown = 1000
     }
 
@@ -305,22 +344,19 @@ function draw() {
     textSize(24)
     text(`${Math.floor(displayFPS)} fps`, width - 16, 16) // Position of the fps label
 
-    // Finalizing frame
-
-    oldTime = millis()
+    oldTime = currentTime
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight)
 }
 
-function audioStarted() {
-    paused = false
+function keyPressed() {
+    if (keyCode === 32) {
+        togglePlayPause()
+    }
 }
 
-function audioStopped() {
-    paused = true
-}
 
 function nextPhoto() {
     imagesSB.push(imagesSB.splice(0, 1)[0])
@@ -329,14 +365,9 @@ function nextPhoto() {
 /* ONGAKU PLAYER FUNCTIONALITIES */
 
 function togglePlayPause() {
-    paused = !paused
-    if (paused) {
-        buttonPlayPause.style.background = "url('images/button-play.svg')"
-        buttonLoop.style.opacity = 0.3
+    if (currentSong.isPlaying()) {
         currentSong.pause()
     } else {
-        buttonPlayPause.style.background = "url('images/button-pause.svg')"
-        buttonLoop.style.opacity = 0.8
         currentSong.play()
     }
 }
@@ -372,4 +403,3 @@ function useDefaultSong() {
 function setSong(song) {
     currentSong = song
 }
-
